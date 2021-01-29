e1696b4fbc21023ec04237c9c4c93fb0537d2e9d
henrytran721 <henryhtran07@gmail.com>
2021-01-28 16:07:33 -0800

    added delete btn to items

--- shopping-cart/src/views/Cart.jsx
+++ shopping-cart/src/views/Cart.jsx
@@ -21,6 +21,15 @@ const Cart = () => {
         setTotal(sum);
     })
 
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }, [cart])
+
+    function handleDelete(e)  {
+        const index = parseInt(e.target.id);
+        setCart(prevCart => prevCart.filter((item) => item.id !== index));
+    }
+
     return (
         <div className='cart-container'>
             {cart.length > 0 ? 
@@ -45,22 +54,23 @@ const Cart = () => {
                                 <div class='order-summary_description'>
                                     <h3>{item.model}</h3>
                                     <p>${item.price}</p>
-                                    <div>
+                                    <div className='order-summary_quantity'>
                                     <span>
-                                        <svg id={index} xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-minus" width="25" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <svg id={index} xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-minus" width="25" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                                         <line x1="5" y1="12" x2="19" y2="12" />
                                         </svg>
                                     </span>
                                     <p>{item.quantity}</p>
                                     <span>
-                                        <svg id={index} xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="25" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <svg id={index} xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="25" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                                         <line x1="12" y1="5" x2="12" y2="19" />
                                         <line x1="5" y1="12" x2="19" y2="12" />
                                         </svg>
                                     </span>
                                     </div>
+                                    <button id={item.id} className='order-summary_delete' onClick={handleDelete}>Delete</button>
                                 </div>
                             </div>
                         )
@@ -78,7 +88,7 @@ const Cart = () => {
                     </div>
                 </div>
             </div>
-            </div> : ''}
+            </div> : 'hello'}
         </div>
     )
 }
